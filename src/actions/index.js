da8ba12e338e043ed068855ad2c39c8148708694
Piotrek <chomackii@gmail.com>
2019-12-04 11:56:48 +0100

    a few refactors - app finished

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,20 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  // chain various methods in one chain
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value(); //exsecute chain
+};
+
 export const fetchPosts = () => async dispatch => {
   // Musi być Thunk (middleware) - bo: BŁąd - Async&await - babel convert ES2015!!!
   const response = await jsonPlaceholder.get("/posts");
@@ -8,10 +22,17 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
-});
+};
+
+// //-lodash memoize method - used only once per one api call
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: response.data });
+// });
